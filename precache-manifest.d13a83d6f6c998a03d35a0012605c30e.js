088f711b32554029ed18b03afc2c59292607229d
Mayank Sinha <mayank.sinha1999@gmail.com>
2020-06-18 13:44:06 +0530

    Updates

---
+++ precache-manifest.d13a83d6f6c998a03d35a0012605c30e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b83a36ced1a032bbb7cae1c7e91478ea",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "18a2a16cff99027482a3",
+    "url": "/hello/static/css/main.c8d522d4.chunk.css"
+  },
+  {
+    "revision": "7e95990b9b6012bf1ac6",
+    "url": "/hello/static/js/2.3aa5db08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hello/static/js/2.3aa5db08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18a2a16cff99027482a3",
+    "url": "/hello/static/js/main.73fa17d9.chunk.js"
+  },
+  {
+    "revision": "f2e48c0cdf4508ab3817",
+    "url": "/hello/static/js/runtime-main.89a0e25e.js"
+  }
+]);
